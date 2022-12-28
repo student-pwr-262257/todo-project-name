1e29f38ece7f84123dadf465e45148d0fef2ec5a
Wojciech_Michalczuk <262241@student.pwr.edu.pl>
2022-12-29 00:14:00 +0100

    Extended Euclid algorithm added

--- todo_project_name/core.py
+++ todo_project_name/core.py
@@ -18,3 +18,16 @@ def mod_exp(b:int, e:int, m:int)->int:
             result=(result*tmp)%m
         tmp=(tmp*tmp)%m
     return result
+
+
+def extended_euclid(a:int,b:int)->tuple:
+    """
+    Function implements extended Euclid algorithm. 
+    Given integers a, b returns integers x, y, such that x*a+y*b==GCD(a,b).
+    """
+    x,y,s,t = 0,1,1,0
+    while a != 0:
+        q,r = b//a,b%a
+        m,n = x-s*q,y-t*q
+        b,a,x,y,s,t = a,r,s,t,m,n
+    return x,y
