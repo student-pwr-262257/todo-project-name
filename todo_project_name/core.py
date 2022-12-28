eddda7a270581a8a7a45d52ba9135f689e82ee1f
Wojciech_Michalczuk <262241@student.pwr.edu.pl>
2022-12-28 16:45:48 +0100

    Fast modular exponetiation

--- todo_project_name/core.py
+++ todo_project_name/core.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
 """Objects needed in many different parts of the package."""
+
+def mod_exp(b:int, e:int, m:int)->int:
+    """
+    Function that performs fast modular exponentiation with given 
+    -b -- base,
+    -e -- exponent,
+    -m -- modulus.
+    """
+    bits=bin(e)[2:]
+    b=b%m
+    tmp=b
+    result=1
+    for i in range(len(bits)):
+        if bits[len(bits)-1-i]=='1':
+            result=(result*tmp)%m
+        tmp=(tmp*tmp)%m
+    return result
