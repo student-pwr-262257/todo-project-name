ca8a7beadc7a82c5dab5b282b4031ea9f477f8ba
Łukasz Pawlak <254654@student.pwr.edu.pl>
2022-12-29 18:15:23 +0100

    Fixed bugs in md4 implementation and its test.
    The bugs in the implementation were hard-to-spot (at least for me)
    typos in 3 different places. In tests, there were typos in checksums,
    and one checksum was missing.
    
    Additionally, moved from using conversion to-and-from bytes using int
    class to using recommended struct module. Replaced some modulo operations
    with faster bit-masking. Improved string representation of message hash.

--- tests/md4_test.py
+++ tests/md4_test.py
@@ -3,14 +3,17 @@ from todo_project_name.md4 import MD4
 # test cases from the paper
 def test_md4():
     # strings encoded as ASCII
-    byte_strings = [b"", b"a", b"abc", b"message digest", b"abcdefghijklmnopqrstuvwxys"]
+    byte_strings = [b"", b"a", b"abc", b"message digest", b"abcdefghijklmnopqrstuvwxyz"]
 
     md4sums = [
         "31d6cfe0d16ae931b73c59d7e0c089c0",
         "bde52cb31de33e46245e05fbdbd6fb24",
-        "a448017aaf21d7525fc10ae87aa6729d",
-        "d73e1c308aa5bbcdeea8ed63df412da9",
+        "a448017aaf21d8525fc10ae87aa6729d",
+        "d9130a8164549fe818874806e1c7014b",
+        "d79e1c308aa5bbcdeea8ed63df412da9",
     ]
 
     for byte_string, md4sum in zip(byte_strings, md4sums):
+        print( MD4(byte_string).string_digest())
+        print( md4sum)
         assert MD4(byte_string).string_digest() == md4sum
--- todo_project_name/md4.py
+++ todo_project_name/md4.py
@@ -1,3 +1,5 @@
+import struct
+import sys
 # some variable names may seem obscure; they were taken directly from
 # the article "The MD4 Message Digest Algorithm" by Ronald L. Rivest
 
@@ -6,35 +8,38 @@ class MD4:
     # magic constants (high-order digits given first == big endian)
     ROUND_2 = 0x5A827999
     ROUND_3 = 0x6ED9EBA1
+
     padding = 0x00000080.to_bytes(64, "little")  # 10000...000 -- 512 bits in total
+    last32 = 0xffffffff
+    last64 = 0xffffffffffffffff
 
     def __init__(self, message: bytes):
         self.digest = None
         # preparing for the algorithm
-        # self.A = 0x01234567 #.to_bytes(4, 'little')
-        # self.B = 0x89abcdef #.to_bytes(4, 'little')
-        # self.C = 0xfedcba98 #.to_bytes(4, 'little')
-        # self.D = 0x76543210 #.to_bytes(4, 'little')
-        self.A = 0x67452301  # this should work on little-endian machines
-        self.B = 0xEFCDAB89
-        self.C = 0x98BADCFE
-        self.D = 0x10325476
+        if sys.byteorder == 'little':
+            self.A = 0x67452301
+            self.B = 0xefcdab89
+            self.C = 0x98badcfe
+            self.D = 0x10325476
+        else: # sys.byteorder == 'big'
+            self.A = 0x01234567
+            self.B = 0x89abcdef
+            self.C = 0xfedcba98
+            self.D = 0x76543210
+        # TODO: do we have to invert byte order in message on big endian?
 
         # running the algorithm
         # in the future it must be converted to operate on stream, in order
-        # to handle hashing large files.
-        X = [None] * 16
+        # to handle hashing large files (idx variable should disappear)
         idx = 0
         bits_no = 0
         byte_no = len(message)
 
         while idx + 64 <= byte_no:
-            for i in range(16):
-                X[i] = int.from_bytes(
-                    message[idx + 4 * i : idx + 4 * (i + 1)], byteorder="little"
-                )
+            X = list(struct.upack("<16I", message[idx: idx + 64])) # 16 unsigned integers
             self._update(X)
             bits_no += 512
+            idx += 64
 
         # padding and running last iteration (or 2 in the case of empty padding)
         message = message[idx:]  # remaining bytes
@@ -44,32 +49,23 @@ class MD4:
         # bytes to append: 56 - left (mod 64)
         if left == 56:  # appending 512 bits.
             message += MD4.padding
-            for i in range(16):
-                X[i] = int.from_bytes(message[4 * i : 4 * (i + 1)], byteorder="little")
+            X = list(struct.unpack("<16I", message[:64]))
             self._update(X)
             message = message[64:]  # only 1 unprocessed pack of 16 words
         else:
             message += MD4.padding[0 : (56 - left) % 64]
             bits_no += left * 8
         # appending number of bits
-        message += (bits_no & 0xFFFFFFFFFFFFFFFF).to_bytes(8, "little")
-        for i in range(16):
-            X[i] = int.from_bytes(message[4 * i : 4 * (i + 1)], byteorder="little")
+        message += struct.pack("<Q", bits_no & MD4.last64) # Q: unsigned long long (8 bytes)
+        X = list(struct.unpack("<16I", message))
         self._update(X)
 
         # getting the result
-        self.digest = b""
-        for chunk in [self.A, self.B, self.C, self.D]:
-            self.digest += chunk.to_bytes(4, "little")
+        self.digest = struct.pack("<4I", self.A, self.B, self.C, self.D)
         # done
 
     def string_digest(self):
-        def convert(val):
-            high = val >> 4  # first 4 bits
-            low = val & 0xF  # last  4 bits
-            return "{:x}{:x}".format(high, low)
-
-        return "".join(map(convert, self.digest))
+        return "".join(f"{byte:02x}" for byte in self.digest)
 
     # bitwise conditional
     @staticmethod
@@ -88,19 +84,19 @@ class MD4:
 
     @staticmethod
     def l_roll(X, s):
-        return ((X << s) & 0xFFFFFFFF) | (X >> (32 - s))
+        return ((X << s) & MD4.last32) | (X >> (32 - s))
 
     @staticmethod
     def round_1_op(A, B, C, D, X, s):
-        return MD4.l_roll(A + MD4.f(B, C, D), s)
+        return MD4.l_roll((A + MD4.f(B, C, D) + X) & MD4.last32, s)
 
     @staticmethod
     def round_2_op(A, B, C, D, X, s):
-        return MD4.l_roll(A + MD4.g(B, C, D) + X + MD4.ROUND_2, s)
+        return MD4.l_roll((A + MD4.g(B, C, D) + X + MD4.ROUND_2) & MD4.last32, s)
 
     @staticmethod
     def round_3_op(A, B, C, D, X, s):
-        return MD4.l_roll(A + MD4.h(B, C, D) + X + MD4.ROUND_3, s)
+        return MD4.l_roll((A + MD4.h(B, C, D) + X + MD4.ROUND_3) & MD4.last32, s)
 
     # X is array with 16 '4-byte words', that is
     # integers whose representation is at most 4 bytes.
@@ -126,18 +122,18 @@ class MD4:
 
         # round 3
         for k in (0, 2, 1, 3):
-            A = MD4.round_2_op(A, B, C, D, X[0 + k], 3)
-            D = MD4.round_2_op(D, A, B, C, X[4 + k], 9)
-            C = MD4.round_2_op(C, D, A, B, X[8 + k], 11)
-            B = MD4.round_2_op(B, C, D, A, X[12 + k], 15)
+            A = MD4.round_3_op(A, B, C, D, X[0 + k], 3)
+            D = MD4.round_3_op(D, A, B, C, X[8 + k], 9)
+            C = MD4.round_3_op(C, D, A, B, X[4 + k], 11)
+            B = MD4.round_3_op(B, C, D, A, X[12 + k], 15)
 
-        self.A = (A + self.A) % (1 << 32)
-        self.B = (B + self.B) % (1 << 32)
-        self.C = (C + self.C) % (1 << 32)
-        self.D = (D + self.D) % (1 << 32)
+        self.A = (A + self.A) & MD4.last32
+        self.B = (B + self.B) & MD4.last32
+        self.C = (C + self.C) & MD4.last32
+        self.D = (D + self.D) & MD4.last32
 
 
 if __name__ == "__main__":
     # quick test for syntax errors
-    x = MD4(b"")
+    x = MD4(b"abcdefghijklmnopqrstuvwxyz")
     print(x.string_digest())
