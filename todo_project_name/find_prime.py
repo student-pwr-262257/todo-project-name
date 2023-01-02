b5cc5837de4993f2eef69b01f02213b313db5494
Wojciech_Michalczuk <262241@student.pwr.edu.pl>
2023-01-03 00:20:00 +0100

    Used random package for witnesses generation

--- todo_project_name/find_prime.py
+++ todo_project_name/find_prime.py
@@ -1,5 +1,6 @@
 import secrets
 from core import mod_exp
+import random
 
 def rabin_miller(candidate:int, repeats:int = 30)->bool:
     """
@@ -27,9 +28,10 @@ def rabin_miller(candidate:int, repeats:int = 30)->bool:
     visited=[]
     for _ in range(repeats):
         flag=False
-        witness=secrets.randbelow(candidate-4)+2
+        #rand a witness in range 2, 3, ..., candidate-1
+        witness=random.randrange(2,candidate)
         while witness in visited:
-            witness=secrets.randbelow(candidate-3)+2
+            witness=random.randrange(2,candidate)
         visited.append(witness)
         tmp=mod_exp(witness, m, candidate)
         if (tmp-1)%candidate==0:
@@ -46,37 +48,39 @@ def find_prime(n:int)->int:
     """
     With high probability returns a n-bit prime number.
     """
-    first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-                        31, 37, 41, 43, 47, 53, 59, 61, 67,
-                        71, 73, 79, 83, 89, 97, 101, 103,
-                        107, 109, 113, 127, 131, 137, 139,
-                        149, 151, 157, 163, 167, 173, 179,
-                        181, 191, 193, 197, 199, 211, 223,
-                        227, 229, 233, 239, 241, 251, 257,
-                        263, 269, 271, 277, 281, 283, 293,
-                        307, 311, 313, 317, 331, 337, 347, 
-                        349, 353, 359, 367, 373, 379, 383, 
-                        389, 397, 401, 409,	419, 421, 431, 
-                        433, 439, 443, 449, 457, 461, 463, 
-                        467, 479, 487, 491, 499, 503, 509, 
-                        521, 523, 541, 547, 557, 563, 569, 
-                        571, 577, 587, 593, 599, 601, 607, 
-                        613, 617, 619, 631, 641, 643, 647, 
-                        653, 659, 661, 673, 677, 683, 691, 
-                        701, 709, 719, 727, 733, 739, 743, 
-                        751, 757, 761, 769, 773, 787, 797, 809]
+    first_primes =  [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                    31, 37, 41, 43, 47, 53, 59, 61, 67,
+                    71, 73, 79, 83, 89, 97, 101, 103,
+                    107, 109, 113, 127, 131, 137, 139,
+                    149, 151, 157, 163, 167, 173, 179,
+                    181, 191, 193, 197, 199, 211, 223,
+                    227, 229, 233, 239, 241, 251, 257,
+                    263, 269, 271, 277, 281, 283, 293,
+                    307, 311, 313, 317, 331, 337, 347, 
+                    349, 353, 359, 367, 373, 379, 383, 
+                    389, 397, 401, 409,	419, 421, 431, 
+                    433, 439, 443, 449, 457, 461, 463, 
+                    467, 479, 487, 491, 499, 503, 509, 
+                    521, 523, 541, 547, 557, 563, 569, 
+                    571, 577, 587, 593, 599, 601, 607, 
+                    613, 617, 619, 631, 641, 643, 647, 
+                    653, 659, 661, 673, 677, 683, 691, 
+                    701, 709, 719, 727, 733, 739, 743, 
+                    751, 757, 761, 769, 773, 787, 797, 809]
     passed=False
     while passed is False:
         flag=True
         candidate=2**(n-1)+secrets.randbits(n-1)
-        if candidate in first_primes_list:
-            return candidate
-        for number in first_primes_list:
+        if candidate<=first_primes[-1]:
+            if candidate in first_primes:
+                return candidate
+            continue
+        for number in first_primes:
             if candidate%number==0:
                 flag=False
         if flag is False:
             continue
 
-        if rabin_miller(candidate):
+        if rabin_miller(candidate,5):
             passed=True
     return candidate
