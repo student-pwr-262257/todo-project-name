ce0a10c998a7dd5c02cadcfbeacbb8576e37e640
Jakub Kaczor <262257@student.pwr.edu.pl>
2023-01-03 01:14:54 +0100

    Acknowledge the new input check in the test

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,11 +8,12 @@ from todo_project_name import core
 
 # Third-party
 import pytest
-from hypothesis import given, strategies as st
+from hypothesis import given, assume, strategies as st
 
 
 @given(st.integers(), st.integers())
 def test_extended_euclid(a, b):
+    assume(a != 0 or b != 0)
     x, y = core.extended_euclid(a, b)
     assert x * a + y * b == math.gcd(a, b)
 
