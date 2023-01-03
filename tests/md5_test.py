f4fe10719691d636edc3fe03f61e7c354ada0236
Łukasz Pawlak <254654@student.pwr.edu.pl>
2023-01-03 21:58:02 +0100

    Adding MD5 class and restructuring MD4.
    Added new class MD5.
    
    Classes MD4 and MD5 now inherit from abstract base class MDN,
    where most common functionally is implemented.
    
    Added test cases for MD5. They DON'T PASS -- there's bug somewhere.
    Perhaps tomorrow...

--- tests/md5_test.py
+++ tests/md5_test.py
@@ -1,28 +1,32 @@
-from todo_project_name.md4 import MD4
+from todo_project_name.md5 import MD5
 
 # test cases from the paper
-def test_md4():
+def test_md5():
     # strings encoded as ASCII
-    byte_strings = [b"", b"a", b"abc", b"message digest", b"abcdefghijklmnopqrstuvwxyz"]
+    byte_strings = [b"", b"a", b"abc", b"message digest", b"abcdefghijklmnopqrstuvwxyz",
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+                    "12345678901234567890123456789012345678901234567890123456789012345678901234567890"]
 
-    md4sums = [
-        "31d6cfe0d16ae931b73c59d7e0c089c0",
-        "bde52cb31de33e46245e05fbdbd6fb24",
-        "a448017aaf21d8525fc10ae87aa6729d",
-        "d9130a8164549fe818874806e1c7014b",
-        "d79e1c308aa5bbcdeea8ed63df412da9",
+    md5sums = [
+        "d41d8cd98f00b204e9800998ecf8427e",
+        "0cc175b9c0f1b6a831c399e269772661",
+        "900150983cd24fb0d6963f7d28e17f72",
+        "f96b697d7cb7938d525a2f31aaf161d0",
+        "c3fcd3d76192e4007dfb496cca67e13b",
+        "d174ab98d277d9f5a5611c2c9f419d9f",
+        "57edf4a22be3c955ac49da2e2107b67a",
     ]
 
-    for byte_string, md4sum in zip(byte_strings, md4sums):
-        assert MD4.from_bytes(byte_string).string_digest() == md4sum
+    for byte_string, md5sum in zip(byte_strings, md5sums):
+        assert MD5.from_bytes(byte_string).string_digest() == md5sum
 
 
 # More test cases, comparing to results computed on the following site:
-# http://www.unit-conversion.info/texttools/md4/
+# http://www.unit-conversion.info/texttools/md5/
 # I noticed that some sites on the internet produce hashes different than ones
 # presented as examples in the paper. The one above however is consistent with it.
 # Perhaps it is fault of text encoding. I assume that the paper uses ASCII.
-def test_extended_md4():
+def test_extended_md5():
     # It is important to check byte strings longer than 56 bytes,
     # as well as ones of length mod 64 equal to 56.
     byte_strings = [
@@ -40,26 +44,26 @@ def test_extended_md4():
         b"officia deserunt mollit anim id est laborum.",
     ]
 
-    md4sums = [
-        "c606c645ee59abf7a5800092024d7655",
-        "a982457b257c736f8d8d1a6e06732337",
-        "18985eb17340f55a0277df1ef3c998ca",
-        "b548c8ffbaf8612c9b317898a18c0ffb",
-        "8db2ba4980fa7d57725e42782ab47b42",
+    md5sums = [
+        "e091d19bae97ef36e24ae8dfd698eb12",
+        "b1efc0fd1c2e6cf52b14a802a855b3c9",
+        "e95e613fc32fb3872bfcaea1bb51d207",
+        "f4be2750a0c444a4f27aabc0eacca237",
+        "956e325fdbfd27759281590bc08315e0",
     ]
-    for byte_string, md4sum in zip(byte_strings, md4sums):
-        assert MD4.from_bytes(byte_string).string_digest() == md4sum
+    for byte_string, md5sum in zip(byte_strings, md5sums):
+        assert MD5.from_bytes(byte_string).string_digest() == md5sum
 
 
 # More test cases, comparing to results computed on the following site:
-# https://www.cmtoinchesconvert.com/online-tools/md4_file_hash.html
+# https://www.cmtoinchesconvert.com/online-tools/md5_file_hash.html
 # It allows to hash files instead of strings of text.
-def test_md4_file():
+def test_md5_file():
     filenames = ["md_test_file.bin", "md_test_file.txt"]
-    md4sums = [
-        "732868172cbed3f7916701c3c289d743",
-        "d3cb716c9993799d7d30ce118fa8d200",
+    md5sums = [
+        "4d413baa3b076bd64e4416a75ca9292f",
+        "9718dafc53ef7fdd3da31171b8706ea3",
     ]
 
-    for filename, md4sum in zip(filenames, md4sums):
-        assert MD4.from_file(filename).string_digest() == md4sum
+    for filename, md5sum in zip(filenames, md5sums):
+        assert MD5.from_file(filename).string_digest() == md5sum
---
+++ todo_project_name/md5.py
@@ -0,0 +1,99 @@
+import struct
+import sys
+from .mdn import MDN
+from math import sin, floor
+
+# some variable names may seem obscure; they were taken directly from
+# the article "The MD5 Message-Digest Algorithm" by Ronald L. Rivest (1992)
+
+
+class MD5(MDN):
+    # magic constants
+    T = list(floor(4294967296 * abs(sin(i + 1))) for i in range(64))
+
+    def __init__(self, message_bytes):
+        super().run_algoritm(message_bytes)
+
+    # bitwise conditional
+    @staticmethod
+    def f(X, Y, Z):
+        return (X & Y) | ((~X) & Z)
+
+    # bitwise conditional
+    @staticmethod
+    def g(X, Y, Z):
+        return (X & Z) | (Y & (~Z))
+
+    # bitwise XOR for 3 inputs (parity function)
+    @staticmethod
+    def h(X, Y, Z):
+        return X ^ Y ^ Z
+
+    @staticmethod
+    def i(X, Y, Z):
+        return Y ^ (X | (~Z))
+
+    # @staticmethod
+    # def round_(round_no):
+    #     fun = {1: MD5.f, 2: MD5.g, 3: MD5.h, 4: MD5.i}[round_no]
+    #     def operation(A, B, C, D, X, s, i):
+    #         return MDN.l_roll((A + fun(B, C, D) + X + MD5.T[i]) & MDN.last32, s)
+    #     return operation
+
+    @staticmethod
+    def round_1_op(A, B, C, D, X, s, i):
+        return (B + MDN.l_roll((A + MD5.f(B, C, D) + X + MD5.T[i]) & MDN.last32, s)) & MDN.last32
+
+    @staticmethod
+    def round_2_op(A, B, C, D, X, s, i):
+        return (B + MDN.l_roll((A + MD5.g(B, C, D) + X + MD5.T[i]) & MDN.last32, s)) & MDN.last32
+
+    @staticmethod
+    def round_3_op(A, B, C, D, X, s, i):
+        return (B + MDN.l_roll((A + MD5.h(B, C, D) + X + MD5.T[i]) & MDN.last32, s)) & MDN.last32
+
+    @staticmethod
+    def round_4_op(A, B, C, D, X, s, i):
+        return (B + MDN.l_roll((A + MD5.i(B, C, D) + X + MD5.T[i]) & MDN.last32, s)) & MDN.last32
+
+    # X is array with 16 '4-byte words', that is
+    # integers whose representation is at most 4 bytes.
+    def _update(self, X) -> bytes:
+        A = self.A  # self.A is `AA` from the paper
+        B = self.B  # the rest accordingly.
+        C = self.C
+        D = self.D
+
+        # round 1
+        for k in range(4):
+            A = MD5.round_1_op(A, B, C, D, X[0 + 4 * k],  7, 0 + 4 * k)
+            D = MD5.round_1_op(D, A, B, C, X[1 + 4 * k], 12, 1 + 4 * k)
+            C = MD5.round_1_op(C, D, A, B, X[2 + 4 * k], 17, 2 + 4 * k)
+            B = MD5.round_1_op(B, C, D, A, X[3 + 4 * k], 22, 3 + 4 * k)
+
+        # round 2
+        for k in range(4):
+            A = MD5.round_2_op(A, B, C, D, X[( 1 + 4 * k) % 16],  5, 16 + 4 * k)
+            D = MD5.round_2_op(D, A, B, C, X[( 6 + 4 * k) % 16],  9, 17 + 4 * k)
+            C = MD5.round_2_op(C, D, A, B, X[(11 + 4 * k) % 16], 14, 18 + 4 * k)
+            B = MD5.round_2_op(B, C, D, A, X[( 0 + 4 * k) % 16], 20, 19 + 4 * k)
+
+        # round 3
+        for k in range(4):
+            A = MD5.round_3_op(A, B, C, D, X[( 1 - 4 * k) % 16],  4, 32 + 4 * k)
+            D = MD5.round_3_op(D, A, B, C, X[( 6 - 4 * k) % 16], 11, 33 + 4 * k)
+            C = MD5.round_3_op(C, D, A, B, X[(11 - 4 * k) % 16], 16, 34 + 4 * k)
+            B = MD5.round_3_op(B, C, D, A, X[( 0 - 4 * k) % 16], 23, 35 + 4 * k)
+
+        # round 4
+        for k in range(4):
+            A = MD5.round_4_op(A, B, C, D, X[( 0 - 4 * k) % 16],  6, 48 + 4 * k)
+            D = MD5.round_4_op(D, A, B, C, X[( 7 - 4 * k) % 16], 10, 49 + 4 * k)
+            C = MD5.round_4_op(C, D, A, B, X[(14 - 4 * k) % 16], 15, 50 + 4 * k)
+            B = MD5.round_4_op(B, C, D, A, X[( 5 - 4 * k) % 16], 21, 51 + 4 * k)
+
+        self.A = (A + self.A) & MDN.last32
+        self.B = (B + self.B) & MDN.last32
+        self.C = (C + self.C) & MDN.last32
+        self.D = (D + self.D) & MDN.last32
+
