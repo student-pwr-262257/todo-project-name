ee2d67eb5440e2b8cda65ca7b7afefc3f726c70a
Jakub Kaczor <262257@student.pwr.edu.pl>
2023-01-04 19:24:49 +0100

    Improve RSA file ops test with strict type check

--- tests/test_rsa.py
+++ tests/test_rsa.py
@@ -9,12 +9,15 @@ import pytest
 
 def test_rsa_file_operations(tmp_path):
     KEY_TYPE = rsa.RSAKeyPublic
+    key_types = {rsa.RSAKeyPublic, rsa.RSAKeyPrivate}
+    other_key_type = key_types - {KEY_TYPE}
 
     key = KEY_TYPE(123, 200)  # TODO: Generate a key. Mock?
     key_path = tmp_path / "test.key"
     rsa.save_key(key, key_path)
     read_key = rsa.read_key(key_path, KEY_TYPE)
     assert key == read_key, "Written and read keys are not the same."
+    assert key != other_key_type, "The exact type should be consistent between reads and writes."
 
 
 def main():
