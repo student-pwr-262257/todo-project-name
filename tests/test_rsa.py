675ad22d62c9e43dfd3488a8941e6f0c477dffb3
Wojciech_Michalczuk <262241@student.pwr.edu.pl>
2023-01-08 00:52:18 +0100

    Tests of rsa_sign, rsa_verify

--- tests/test_rsa.py
+++ tests/test_rsa.py
@@ -2,7 +2,8 @@
 
 # First-party
 from todo_project_name import rsa
-
+from todo_project_name.md4 import MD4
+from todo_project_name.md5 import MD5
 # Third-party
 import pytest
 
@@ -19,6 +20,56 @@ def test_rsa_file_operations(tmp_path):
     assert key == read_key, "Written and read keys are not the same."
     assert key != other_key_type, "The exact type should be consistent between reads and writes."
 
+
+def test_rsa_sign():
+
+    #test for example messages, key_lengths, hash methods
+    messages = ["s","a","*","qwerty", "q1w2e3r4t5y6u7i8o9p0", "!@#$%^&*()oooooo", 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+    """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+    officia deserunt mollit anim id est laborum."""]
+    key_sizes = [32, 64, 64, 128, 256, 128, 256, 256]
+    algorithms = [MD4, MD5, MD5, MD4, MD4, MD5, MD4, MD4]
+    for message, key_size, algorithm in zip(messages, key_sizes, algorithms):
+        keys_pair = rsa.rsa_key_gen(key_size)
+        signature = rsa.rsa_sign(message, keys_pair.private, algorithm)
+        signature = int(signature, 16)
+        assert signature < keys_pair.private.modulus
+
+        #decode and compare
+        hashed=algorithm.from_bytes(message.encode('utf-8')).string_digest()
+        hashed=int(hashed, 16) % keys_pair.private.modulus
+        assert hashed == pow(signature, keys_pair.public.key, keys_pair.public.modulus)
+
+    #the case of trivial key
+    for message, key_size in zip(messages, key_sizes):
+        key = rsa.RSAKeyPrivate(1, 2 * key_size)
+        hashed = MD4.from_bytes(message.encode('utf-8')).string_digest()
+        assert int(rsa.rsa_sign(message, key), 16) == int(hashed, 16) % key.modulus
+
+def test_rsa_verify():
+
+    #example messages, key_lengths, hash methods
+    messages = ["","a","*","qwerty", "q1w2e3r4t5y6u7i8o9p0", "!@#$%^&*()oooooo", 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+    """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+    officia deserunt mollit anim id est laborum.""", "LOREM IPSUM"]
+    key_sizes = [32, 64, 64, 128, 256, 128, 256, 256, 64]
+    algorithms = [MD4, MD5, MD5, MD4, MD4, MD5, MD4, MD4, MD4]
+    for message, key_size, algorithm in zip(messages, key_sizes, algorithms):
+        key = rsa.rsa_key_gen(key_size)
+        signature = rsa.rsa_sign(message, key.private, algorithm)
+        assert rsa.rsa_verify(message, signature, key.public, algorithm)
 
 def main():
     pytest.main([__file__])
