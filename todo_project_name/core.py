fc0b7571ca28194ffaa71837cb6d73cb3655f49f
Łukasz Pawlak <254654@student.pwr.edu.pl>
2023-01-08 14:56:00 +0100

    Adding functions `md*_string`
    It is not very readable to use
    `MD4.from_bytes(message.encode("utf-8")).string_digest()`
    so this one-line is now just `md4_string(message)`.
    Same for md5.

--- todo_project_name/core.py
+++ todo_project_name/core.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
+from .md4 import MD4
+from .md5 import MD5
 
 """Objects needed in many different parts of the package."""
+
+
+def md4_string(message: str) -> str:
+    """Returns md4 digest of given string encoded as UTF-8 byte strings.
+
+    Parameters
+    ==========
+    message
+    : string whose hash is to be computed.
+    """
+    return MD4.from_bytes(message.encode("utf-8")).string_digest()
+
+
+def md5_string(message: str) -> str:
+    """Returns md5 digest of given string encoded as UTF-8 byte strings.
+
+    Parameters
+    ==========
+    message
+    : string whose hash is to be computed.
+    """
+    return MD5.from_bytes(message.encode("utf-8")).string_digest()
